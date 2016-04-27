2ffbc79670869fa94e48f322034002a7ad189ff9
Sion Williams <sion@byteshifter.io>
2016-04-27 11:12:17 +0100

    fix: add @Fetch annotation so that the correct details are returned instead of null at http://127.0.0.1:8080/dependency/0/dependencies

--- src/main/groovy/io/byteshifter/depsgraph/domain/Dependency.groovy
+++ src/main/groovy/io/byteshifter/depsgraph/domain/Dependency.groovy
@@ -1,48 +1,46 @@
 package io.byteshifter.depsgraph.domain
 
-import org.neo4j.graphdb.Direction
 import org.springframework.data.neo4j.annotation.Fetch
 import org.springframework.data.neo4j.annotation.GraphId
 import org.springframework.data.neo4j.annotation.NodeEntity
 import org.springframework.data.neo4j.annotation.RelatedTo
 
+import static org.neo4j.graphdb.Direction.OUTGOING
+
+// Example Payload
+//{
+//    "groupId": "group3",
+//    "artifactId": "artifact3",
+//    "version": "1.0",
+//    "dependencies": ["http://127.0.0.1:8080/dependency/0", "http://127.0.0.1:8080/dependency/1", "http://127.0.0.1:8080/dependency/2"]
+//}
+
 /**
  * Dependency Node Entity.
- * The Dependency class is annotated {@code @NodeEntity}. When Neo4j stores it, it results in the creation of a new node.
  * @author Sion Williams
  */
 @NodeEntity
 class Dependency {
-    // Coordinates
-    @GraphId Long id
+    @GraphId
+    private Long id
+
+    // Maven Coordinates
     String groupId
     String artifactId
     String version
-//
-//    @Fetch
-//    @RelatedTo(type = "DEPENDS_ON", direction = Direction.OUTGOING)
-//    public Set<Dependency> dependencies
-//
-//    /**
-//     * Used to link dependencies together with Direction.OUTGOING
-//     *
-//     * @param dependency Another dependency entity
-//     */
-//    public void dependsOn(Dependency dependency) {
-//        if ( !dependencies ) {
-//            dependencies = new HashSet<Dependency>()
-//        }
-//        dependencies.add(dependency)
-//    }
-//
-//    @Override
-//    String toString() {
-//        String results = groupId + " " + artifactId + " " + version + "\n"
-//        if (dependencies) {
-//            dependencies.each {
-//                results += "\t-" + it.groupId + " " + it.artifactId + " " + it.version + "\n"
-//            }
-//        }
-//        results
-//    }
+
+    @RelatedTo(type = "DEPENDS_ON", direction = OUTGOING)
+    @Fetch
+    Set<Dependency> dependencies
+
+    @Override
+    String toString() {
+        return "Dependency{" +
+                "id=" + id +
+                ", groupId='" + groupId + '\'' +
+                ", artifactId='" + artifactId + '\'' +
+                ", version='" + version + '\'' +
+                ", dependencies=" + dependencies +
+                '}';
+    }
 }
